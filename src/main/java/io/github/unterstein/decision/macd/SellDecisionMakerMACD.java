dfd7b8603ca7186a91952ad831b456d9de2fde2c
Sergii Prykhodko <sergii.pryhodko@gmail.com>
2018-06-28 08:43:18 +0300

    added sell logic to MACD strategy

--- src/main/java/io/github/unterstein/decision/macd/SellDecisionMakerMACD.java
+++ src/main/java/io/github/unterstein/decision/macd/SellDecisionMakerMACD.java
@@ -1,4 +1,14 @@
 package io.github.unterstein.decision.macd;
 
+import io.github.unterstein.statistic.MACD.MACD;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class SellDecisionMakerMACD {
+
+    @Autowired
+    private MACD macd;
+
+    public boolean isMACDAscending() {
+        return macd.isAccending();
+    }
 }
--- src/main/java/io/github/unterstein/strategy/MACDStrategy.java
+++ src/main/java/io/github/unterstein/strategy/MACDStrategy.java
@@ -1,14 +1,15 @@
 package io.github.unterstein.strategy;
 
 import io.github.unterstein.TradingClient;
-import io.github.unterstein.decision.maandrsi.BuyDecisionMaker;
-import io.github.unterstein.decision.maandrsi.SellDecisionMaker;
 import io.github.unterstein.decision.macd.BuyDecisionMakerMACD;
 import io.github.unterstein.decision.macd.SellDecisionMakerMACD;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import static io.github.unterstein.remoteManagment.ManagementConstants.stopTicker;
+import static util.Slepper.sleepSeconds;
 
 @Component
 public class MACDStrategy implements Strategy {
@@ -46,7 +47,46 @@ public class MACDStrategy implements Strategy {
 
     @Override
     public void sellProcess() {
+        sleepSeconds(180);
+        goalSellPrice = boughtPrice + (boughtPrice * 0.002);
+        while (isUpTrending()) {
+            sleepSeconds(3);
+            updateLastBid();
+            if (lastBid > goalSellPrice){
+                wasOverGoalPrice = true;
+            }
+            if (stopTicker){
+                return;
+            }
+        }
+        if (wasOverGoalPrice && lastBid > minimumProfitablePrice()){
+            logger.info("MACD trend changed and price was over goal price!");
+            sellToMarket(lastBid);
+            return;
+        }
+    }
 
+    private boolean isUpTrending() {
+        return sellDecisionMaker.isMACDAscending();
+    }
+
+    private void updateLastBid() {
+        lastBid = getLastBid();
+    }
+
+    private double getLastBid() {
+        return client.getLastBidsAverage(tradeAmount, 3);
+    }
+
+    private void sellToMarket(Double lastBid) {
+        client.sellMarket(tradeAmount);
+        logger.info(String.format("Sold %d coins to market! Rate: %.8f", tradeAmount, lastBid));
+        logger.info(String.format("Profit %.8f", (boughtPrice - lastBid) * tradeAmount));
+        isBought = false;
+    }
+
+    private double minimumProfitablePrice() {
+        return boughtPrice + (boughtPrice * 0.0015);
     }
 
     @Override
